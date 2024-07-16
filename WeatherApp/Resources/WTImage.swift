f6328a347d4ef4d7e93de446dbbdd286309c9ce8
kwakseobang <iii6602@naver.com>
2024-07-16 17:23:31 +0900

    .

---
+++ WeatherApp/Resources/WTImage.swift
@@ -0,0 +1,14 @@
+//
+//  WTImage.swift
+//  WeatherApp
+//
+//  Created by 곽서방 on 7/16/24.
+//
+
+import Foundation
+import SwiftUI
+enum WTImage {
+    static var sun: Image = Image(systemName: "sun.max")
+    
+    
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 곽서방 on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
